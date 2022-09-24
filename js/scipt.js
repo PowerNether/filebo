9c050cf371b9427960b83e73639d6fef7fabcf52
PowerNether <slavacraft07@gmail.com>
2022-09-25 00:54:03 +0300

    fix

--- js/scipt.js
+++ js/scipt.js
@@ -1,15 +1,50 @@
 function stickyHeader () {
-    const header = document.querySelector('.header') || null;
+    const header = document.querySelector('.header');
 
     window.addEventListener('scroll', function () {
         if (window.scrollY > 868) {
-            header.classList.add('sticky');
+            if (window.innerWidth > 768) {
+                header.classList.add('sticky');
+            }
         } else {
             header.classList.remove('sticky');
         }
     }, { passive: true })
 }
 stickyHeader();
+
+function openHeader () {
+    const header = document.querySelector('.header');
+    const toggler = document.querySelector('.header__toggler');
+
+    toggler.addEventListener('click', function () {
+        header.classList.contains('open') ? header.classList.remove('open') : header.classList.add('open');
+    })
+}
+openHeader();
+
+function linkScroll () {
+    const header = document.querySelector('.header');
+    const links = Array.from(document.querySelectorAll('.hedear__link')) || [];
+
+    links.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            let anchor = link.getAttribute('href').substr(1);
+            let anchorBlock = document.getElementById(anchor)
+
+            let blockTop = anchorBlock.getBoundingClientRect();
+            
+            window.scrollTo({
+                top: blockTop.top + window.pageYOffset - 104,
+                behavior: 'smooth',
+            });
+
+            header.classList.remove('open');
+        });
+    });
+}
+linkScroll();
 
 MicroModal.init();
 
